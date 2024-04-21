41afcdde5bb6710f65d226ef61d624519aaf12a5
Ganesh Kumar <mrganeshofficial8@gmail.com>
2024-04-21 13:18:29 +0530

    made changes

--- app.js
+++ app.js
@@ -9,6 +9,25 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    
+    "http://localhost:4200",
+    "https://management-project-iq4n.onrender.com",  
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key,If-Modified-Since,Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  return next();
+});
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +39,10 @@ app.use(express.urlencoded({ extended: false }));
 userRouter.use(express.json());
 app.use("/users", userRouter);
 app.use("/task", taskRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 
 const port = process.env.PORT || 4093;
